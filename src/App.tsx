3ef164383ed96889031c514864dd48d0ef37fe15
tateisnoone <tatdagelo@gmail.com>
2024-09-22 23:36:24 +0400

    assignment

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+import CardsSection from "./Components/CardsSection";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Hero />
+        <CardsSection />
+      </div>
+    </>
+  );
+}
+
+export default App;
