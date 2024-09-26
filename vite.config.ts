6311affc68e6b42db5b433c23d8e9d8b16284485
tateisnoone <tatdagelo@gmail.com>
2024-09-27 02:25:11 +0400

    pushing config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(() => {
+  const rootPath = path.resolve(process.cwd());
+  const srcPath = `${rootPath}/src`;
+  const componentPath = `${rootPath}/src/Components`;
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "~": rootPath,
+        "@": srcPath,
+        "#": componentPath,
+      },
+    },
+  };
+});
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
