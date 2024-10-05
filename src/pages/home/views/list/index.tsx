151c10917cd9f28dfe590a1ffaef58f094b6c237
tateisnoone <tatdagelo@gmail.com>
2024-10-06 01:55:56 +0200

    fixed typo

--- src/pages/home/views/list/index.tsx
+++ src/pages/home/views/list/index.tsx
@@ -8,7 +8,7 @@ const LazyCardSection = lazy(
   () => import("../../components/card-list/card-section")
 );
 
-const LazyHero = lazy(() => import("../../components/hero/hero"));
+const LazyHero = lazy(() => import("../../components/hero"));
 
 const CardsListView = () => {
   return (
