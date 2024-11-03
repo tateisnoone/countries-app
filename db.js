181fd62a56158eb74ba31e658bbbbfc38df679dd
tateisnoone <tatdagelo@gmail.com>
2024-11-03 23:38:33 +0100

    added seed-db and posting to my json db from there

--- db.js
+++ db.js
@@ -1,10 +1,17 @@
 import axios from "axios";
 
-axios
-    .get("https://restcountries.com/v3.1/all")
-    .then((res) => {
-        console.log(res.data);
-    })
-    .catch((error) => {
-        console.error("Error fetching data:", error);
+axios.get("https://restcountries.com/v3.1/all").then((res) => {
+    const countriesData = res.data.map((country) => ({
+        id: country.cca2,
+        name: country.name.common,
+        nameGe: country.name.official,
+        population: country.population,
+        capital: country.capital ? country.capital[0] : "N/A",
+        capitalGe: country.capital ? country.capital[0] : "N/A",
+        image: country.flags.svg || "",
+        vote: 0,
+    }));
+    countriesData.forEach((country) => {
+        axios.post("http://localhost:3000/countries", country);
     });
+});
